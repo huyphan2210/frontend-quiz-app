3997c7c6694410f08a498b7f44a7c80a496cc315
HuyPhanNC <huy.phan2210@outlook.com>
2025-02-07 11:49:25 +0700

    Initialize a Welcome Page

---
+++ frontend-quiz-app.client/src/pages/Welcome/Welcome.page.tsx
@@ -0,0 +1,37 @@
+import { FC, useLayoutEffect, useState } from "react";
+import "./Welcome.page.scss";
+import { getQuizCategories } from "../../services/Quiz.service";
+import { QuizCategoryResponse } from "../../api";
+
+const WelcomePage: FC = () => {
+  const [quizCategories, setQuizCategories] = useState<QuizCategoryResponse[]>(
+    []
+  );
+
+  useLayoutEffect(() => {
+    getQuizCategories().then((quizCategories) =>
+      setQuizCategories(quizCategories)
+    );
+  }, []);
+  return (
+    <>
+      <h1>
+        Welcome to the <span>Frontend Quiz!</span>
+      </h1>
+      <small>
+        <i>Pick a subject to get started</i>
+      </small>
+      <ul>
+        {quizCategories.map((category, i) => (
+          <li key={i}>
+            <button type="button">
+              <img src={category.imgUrl} loading="lazy" alt={category.name} />
+            </button>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default WelcomePage;
