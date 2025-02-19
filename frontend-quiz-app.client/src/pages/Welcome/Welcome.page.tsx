286446cfb422eae93e0cfe9b479335cb0af6e07f
HuyPhanNC <huy.phan2210@outlook.com>
2025-02-19 13:06:15 +0700

    Change button -> a

--- frontend-quiz-app.client/src/pages/Welcome/Welcome.page.tsx
+++ frontend-quiz-app.client/src/pages/Welcome/Welcome.page.tsx
@@ -1,12 +1,20 @@
-import { FC, useLayoutEffect, useState } from "react";
+import { FC, MouseEventHandler, useLayoutEffect, useState } from "react";
 import "./Welcome.page.scss";
 import { getQuizCategories } from "../../services/Quiz.service";
 import { QuizCategoryResponse } from "../../api";
+import { useNavigate } from "react-router-dom";
+import { Pages } from "../../utilities/global-var";
 
 const WelcomePage: FC = () => {
+  const navigation = useNavigate();
   const [quizCategories, setQuizCategories] = useState<QuizCategoryResponse[]>(
     []
   );
+
+  const handleNavigation: MouseEventHandler<HTMLAnchorElement> = (e) => {
+    e.preventDefault();
+    navigation(e.currentTarget.pathname + e.currentTarget.search);
+  };
 
   useLayoutEffect(() => {
     getQuizCategories().then((quizCategories) =>
@@ -26,10 +34,14 @@ const WelcomePage: FC = () => {
       <ul className="category_list">
         {quizCategories.map((category, i) => (
           <li key={i}>
-            <button type="button" className='category'>
+            <a
+              onClick={handleNavigation}
+              href={`${Pages.Quiz}?category=${category.name?.toLowerCase()}`}
+              className="category"
+            >
               <img src={category.imgUrl} loading="lazy" alt={category.name} />
               <span>{category.name}</span>
-            </button>
+            </a>
           </li>
         ))}
       </ul>
