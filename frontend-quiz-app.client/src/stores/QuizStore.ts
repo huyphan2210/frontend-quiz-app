edabd6b70ccd7ef31f6e0ae70d0cc4b10f34a3cd
HuyPhanNC <huy.phan2210@outlook.com>
2025-02-24 11:12:03 +0700

    Use quizService.ts in QuizStore

--- frontend-quiz-app.client/src/stores/QuizStore.ts
+++ frontend-quiz-app.client/src/stores/QuizStore.ts
@@ -1,19 +1,40 @@
 import { action, makeAutoObservable, observable } from "mobx";
 import { createContext } from "react";
-import { QuizCategoryResponse } from "../api";
+import { EQuizCategory, QuizCategoryResponse, QuizResponse } from "../api";
+import {
+  getQuizzesByCategory,
+  getQuizCategories as apiGetQuizCategories,
+} from "../services/Quiz.service";
 
 export default class QuizStore {
   currentQuizCategory?: QuizCategoryResponse;
+  currentQuizzes?: QuizResponse[];
+  quizCategories?: QuizCategoryResponse[];
 
   constructor() {
     makeAutoObservable(this, {
       currentQuizCategory: observable,
-      setQuizCategory: action,
+      getQuizCategories: action,
+      setCurrentQuizCategory: action,
     });
   }
 
-  setQuizCategory(quizCategory?: QuizCategoryResponse) {
+  getQuizCategories() {
+    return apiGetQuizCategories().then((categories) => {
+      this.quizCategories = categories;
+    });
+  }
+
+  setCurrentQuizCategory(quizCategory?: QuizCategoryResponse) {
     this.currentQuizCategory = quizCategory;
+    if (this.currentQuizCategory?.type)
+      return this.getQuizzesByCategory(this.currentQuizCategory?.type);
+  }
+
+  getQuizzesByCategory(categoryType: EQuizCategory) {
+    return getQuizzesByCategory(categoryType).then(
+      (quizzes) => (this.currentQuizzes = quizzes)
+    );
   }
 }
 
