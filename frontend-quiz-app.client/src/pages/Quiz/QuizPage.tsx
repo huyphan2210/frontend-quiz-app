2f6316fe99f903cb68f257798312d73020595a5e
HuyPhanNC <huy.phan2210@outlook.com>
2025-02-26 13:22:05 +0700

    Start to style QuizPage

--- frontend-quiz-app.client/src/pages/Quiz/QuizPage.tsx
+++ frontend-quiz-app.client/src/pages/Quiz/QuizPage.tsx
@@ -37,7 +37,27 @@ const QuizPage: FC = () => {
     setCurrentQuizCategory();
   }, []);
 
-  return <></>;
+  return (
+    <>
+      <small className="quiz_small-text">
+        <i>
+          Question {currentQuiz?.order} of{" "}
+          {quizStore.currentQuizzes?.length || 0}
+        </i>
+      </small>
+      <h1 className="quiz_question">{currentQuiz?.question}</h1>
+      <form className="quiz_form">
+        <ul className="quiz_form_option-list">
+          {currentQuiz?.options?.map((option, index) => (
+            <li className="quiz_form_option-list_choice" key={index}>
+              <input id={`option-${index}`} type="checkbox"></input>
+              <label htmlFor={`option-${index}`}>{option}</label>
+            </li>
+          ))}
+        </ul>
+      </form>
+    </>
+  );
 };
 
 const QuizPageObserver = QuizPage;
