e45e76c1c9f9ab5818c8fd857aefff83b5e1349b
HuyPhanNC <huy.phan2210@outlook.com>
2025-03-03 13:44:24 +0700

    Add new DTOs

---
+++ frontend-quiz-app.Server/DTOs/Request/QuizRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace frontend_quiz_app.Server.DTOs.Request
+{
+    public class QuizRequest
+    {
+        public required string EncryptKeyBase64 { get; set; }
+    }
+}
---
+++ frontend-quiz-app.Server/DTOs/TextEncryptedData.cs
@@ -0,0 +1,8 @@
+﻿namespace frontend_quiz_app.Server.DTOs
+{
+    public class TextEncryptedData
+    {
+        public required string Encrypted { get; set; }
+        public required string Iv { get; set; }
+    }
+}
