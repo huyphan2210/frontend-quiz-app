057c908d6d7cabde9a90cc58d4704a8e84e09451
HuyPhanNC <huy.phan2210@outlook.com>
2025-03-03 13:46:23 +0700

    Update IQuizService.cs

--- frontend-quiz-app.Server/Services/IQuizService.cs
+++ frontend-quiz-app.Server/Services/IQuizService.cs
@@ -1,11 +1,12 @@
-﻿using frontend_quiz_app.Server.DTOs;
-using frontend_quiz_app.Server.DTOs.Enums;
+﻿using frontend_quiz_app.Server.DTOs.Enums;
+using frontend_quiz_app.Server.DTOs.Request;
+using frontend_quiz_app.Server.DTOs.Responses;
 
 namespace frontend_quiz_app.Server.Services
 {
     public interface IQuizService
     {
         public IList<QuizCategoryResponse> GetQuizCategories();
-        public IList<QuizResponse> GetQuizzesByCategory(EQuizCategory quizCategory);
+        public IList<QuizResponse> GetQuizzesByCategory(EQuizCategory quizCategory, QuizRequest quizRequest);
     }
 }
