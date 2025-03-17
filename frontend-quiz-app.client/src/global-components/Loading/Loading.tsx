e70891919d602f5949cd71c4dab86ff88d24db9c
HuyPhanNC <huy.phan2210@outlook.com>
2025-03-17 16:18:12 +0700

    Setup Loading component

--- frontend-quiz-app.client/src/global-components/Loading/Loading.tsx
+++ frontend-quiz-app.client/src/global-components/Loading/Loading.tsx
@@ -1,10 +1,58 @@
-import { FC } from "react";
+import { FC, useEffect, useRef, useState } from "react";
 import "./Loading.scss";
 
-const Loading: FC = () => {
-  return <div className="loading">
-    Loading...
-  </div>;
+interface ILoading {
+  isLoading?: boolean;
+}
+
+const Loading: FC<ILoading> = ({ isLoading }) => {
+  const SECONDS_LIMIT = 30;
+  const [secondsPassed, setSecondPassed] = useState(0);
+  const modal = useRef<HTMLDialogElement>(null);
+
+  useEffect(() => {
+    if (!isLoading) {
+      setSecondPassed(SECONDS_LIMIT);
+      modal.current?.classList.add("close");
+      setTimeout(() => {
+        modal.current?.close();
+        setSecondPassed(0);
+        modal.current?.classList.remove("close");
+      }, 1500);
+      return;
+    }
+    modal.current?.showModal();
+    const intervalIndex = setInterval(() => {
+      setSecondPassed((prev) => prev + 1);
+    }, 1000);
+
+    let secondIntervalIndex = 0;
+
+    setTimeout(() => {
+      clearInterval(intervalIndex);
+      secondIntervalIndex = setInterval(() => {
+        setSecondPassed((prev) => prev + 0.1);
+      }, 1000);
+      setTimeout(() => {
+        clearInterval(secondIntervalIndex);
+      }, 40000);
+    }, 25000);
+
+    return () => {
+      clearInterval(intervalIndex);
+      clearInterval(secondIntervalIndex);
+    };
+  }, [isLoading]);
+
+  return (
+    <dialog ref={modal} className="loading">
+      <progress
+        className="loading_time-bar"
+        value={secondsPassed}
+        max={SECONDS_LIMIT}
+      ></progress>
+    </dialog>
+  );
 };
 
 export default Loading;
