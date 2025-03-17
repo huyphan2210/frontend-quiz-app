8ca2902f0d093e384c17ba3b8ebe4623b81ae476
HuyPhanNC <huy.phan2210@outlook.com>
2025-03-17 16:17:17 +0700

    Move the initilization of axios to Layout and add interceptors

--- frontend-quiz-app.client/src/Layout.tsx
+++ frontend-quiz-app.client/src/Layout.tsx
@@ -1,15 +1,54 @@
+import axios from "axios";
+import { serviceOptions } from "./api";
+import Loading from "./global-components/Loading/Loading";
 import Header from "./layout/Header/Header";
 import Main from "./layout/Main/Main";
+import { CheckAndReturnLoadingStore } from "./utilities/storeHelper";
+import { observer } from "mobx-react";
 
 function Layout() {
+  const loadingStore = CheckAndReturnLoadingStore();
+
+  serviceOptions.axios = axios.create({
+    baseURL: import.meta.env.BASE_URL,
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+  });
+
+  serviceOptions.axios.interceptors.request.use(
+    (config) => {
+      loadingStore.setIsLoading(true);
+      return config;
+    },
+    (error) => {
+      return Promise.reject(error);
+    }
+  );
+
+  serviceOptions.axios.interceptors.response.use(
+    (config) => {
+      setTimeout(() => {
+        loadingStore.setIsLoading(false);
+      }, 1000);
+      return config;
+    },
+    (error) => {
+      return Promise.reject(error);
+    }
+  );
+
   return (
     <>
       <Header />
       <Main />
-      <footer>
-        Challenged by Huy Phan
-      </footer>
+      <footer>Challenged by Huy Phan</footer>
+      <Loading isLoading={loadingStore.isLoading} />
     </>
   );
 }
-export default Layout;
+
+const LayoutObserver = observer(Layout);
+
+export default LayoutObserver;
--- frontend-quiz-app.client/src/main.tsx
+++ frontend-quiz-app.client/src/main.tsx
@@ -3,18 +3,10 @@ import { createRoot } from "react-dom/client";
 import "./styles/default.scss";
 import Layout from "./Layout";
 
-import axios from "axios";
-import { serviceOptions } from "./api";
 import { BrowserRouter } from "react-router-dom";
 import StoreProvider from "./stores/StoreProvider";
 
-serviceOptions.axios = axios.create({
-  baseURL: import.meta.env.BASE_URL,
-  headers: {
-    Accept: "application/json",
-    "Content-Type": "application/json",
-  },
-});
+
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
